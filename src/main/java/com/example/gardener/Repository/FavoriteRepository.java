3c2b749f406b53639ad7349843b451eeede00b96
kozodoy <checksmetanin@yandex.ru>
2025-11-08 20:24:10 +0300

    misk_for_plan_architecture

--- src/main/java/com/example/gardener/Repository/FavoriteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gardener.Repository;
-
-import com.example.gardener.Entities.Favorite;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
-}
