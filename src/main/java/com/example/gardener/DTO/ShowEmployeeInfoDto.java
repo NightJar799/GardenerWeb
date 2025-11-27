dd5c6fa2ac71a352641b990bdfbbc6458aa45105
kozodoy <checksmetanin@yandex.ru>
2025-11-27 23:22:15 +0300

    cleaned_and_added_meanFiled_comtroller

--- src/main/java/com/example/gardener/DTO/ShowEmployeeInfoDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.gardener.DTO;
-
-public class ShowEmployeeInfoDto {
-}
--- src/main/java/com/example/gardener/controller/PlantListController.java
+++ src/main/java/com/example/gardener/controller/PlantListController.java
@@ -1,4 +1,15 @@
 package com.example.gardener.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
 public class PlantListController {
+    @GetMapping("/")
+    public String listPlantsGet() {
+        log.info("получаем главную страницу");
+        return "mainGarden";
+    }
 }
